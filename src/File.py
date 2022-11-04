7d2fec550a982e9ca2308f198a1d0fd6ad4a75aa
srpurba <srpurba@myseneca.ca>
2022-11-03 20:52:34 -0400

    fix: pylint warnings has been fixed

--- src/File.py
+++ src/File.py
@@ -1,7 +1,7 @@
 import shutil
-import markdown2
 from os import listdir, mkdir
 from os.path import isfile, join, isdir
+import markdown2
 from src import Utils
 
 
@@ -40,24 +40,24 @@ class SSG:
                 # directory list styles
                 Utils.writeCSSToFile(file)
 
-    def start(self, input, output=None):
+    def start(self, input_path, output=None):
         if Utils.checkIfOutFlag(self.output):
             output = self.output if output is None else output
         else:
             output = self.output + "/dist" if output is None else output
-        if isfile(input) and input.endswith(".txt"):
+        if isfile(input_path) and input_path.endswith(".txt"):
             # single file, index of the static site
-            self.process_file(input, output.rstrip("/") + "/index.txt")
-        elif isfile(input) and input.endswith(".md"):
+            self.process_file(input_path, output.rstrip("/") + "/index.txt")
+        elif isfile(input_path) and input_path.endswith(".md"):
             # single file, index of the static site
-            self.process_md_file(input, output.rstrip("/") + "/index.md")
-        elif isdir(input):
-            self.process_dir(input, output)
+            self.process_md_file(input_path, output.rstrip("/") + "/index.md")
+        elif isdir(input_path):
+            self.process_dir(input_path, output)
         else:
-            Utils.errLog("Invalid input file")
+            Utils.errLog("Invalid input_path file")
 
-    def process_file(self, input, output):
-        with open(input, "r", encoding="utf-8") as file:
+    def process_file(self, input_path, output):
+        with open(input_path, "r", encoding="utf-8") as file:
             lines = file.readlines()
         title = output.split("/")[-1][:-4]  # filename
         title = title if title != "index" else output.split("/")[-2]
@@ -66,7 +66,7 @@ class SSG:
             lines = lines[3:]
             print(">> ", output.split("/")[-1][:-4])
         except IndexError:
-            if len(lines) == 0 and input.endswith(".txt"):
+            if len(lines) == 0 and input_path.endswith(".txt"):
                 Utils.errLog(f"Empty file passed: {title}.txt")
 
         with open(output[:-4] + ".html", "w", encoding="utf-8") as file:
@@ -96,8 +96,14 @@ class SSG:
             file.write(f"""<h1>{title}</h1>\n""")
             file.write("""<div class="content">\n""")
             last_i = 0
-            for i in range(len(lines)):
-                if lines[i] == "\n":
+            # for i in range(len(lines)):
+            #     if lines[i] == "\n":
+            #         file.write("""<p>\n""")
+            #         file.write(" ".join(lines[last_i:i]))
+            #         file.write("""</p>\n""")
+            #         last_i = i + 1
+            for i, line in enumerate(lines):
+                if line == "\n":
                     file.write("""<p>\n""")
                     file.write(" ".join(lines[last_i:i]))
                     file.write("""</p>\n""")
@@ -113,8 +119,8 @@ class SSG:
 
             file.write("""</html>""")
 
-    def process_md_file(self, input, output):
-        md_content = markdown2.markdown_path(input, extras=["fenced-code-blocks"])
+    def process_md_file(self, input_path, output):
+        md_content = markdown2.markdown_path(input_path, extras=["fenced-code-blocks"])
 
         title = output.split("/")[-1][:-3]  # index
         title = title if title != "index" else output.split("/")[-2]
@@ -152,29 +158,37 @@ class SSG:
 
             file.write("""</html>""")
 
-    def process_dir(self, input, output):
-        # directories in input folder
-        onlydir = sorted([f for f in listdir(input) if isdir(join(input, f))])
+    def process_dir(self, input_path, output):
+        # directories in input_path folder
+        onlydir = sorted([f for f in listdir(input_path) if isdir(join(input_path, f))])
 
-        # files in input folder
+        # files in input_path folder
         onlytxt = sorted(
-            [f for f in listdir(input) if isfile(join(input, f)) and f.endswith(".txt")]
+            [
+                f
+                for f in listdir(input_path)
+                if isfile(join(input_path, f)) and f.endswith(".txt")
+            ]
         )
         onlymd = sorted(
-            [f for f in listdir(input) if isfile(join(input, f)) and f.endswith(".md")]
+            [
+                f
+                for f in listdir(input_path)
+                if isfile(join(input_path, f)) and f.endswith(".md")
+            ]
         )
         # recursively process directories
         for directory in onlydir:
             mkdir(join(output, directory))
-            self.process_dir(join(input, directory), join(output, directory))
+            self.process_dir(join(input_path, directory), join(output, directory))
 
         # process txt files
         for txtfile in onlytxt:
-            self.process_file(join(input, txtfile), join(output, txtfile))
+            self.process_file(join(input_path, txtfile), join(output, txtfile))
 
         # process md files
         for mdfile in onlymd:
-            self.process_md_file(join(input, mdfile), join(output, mdfile))
+            self.process_md_file(join(input_path, mdfile), join(output, mdfile))
 
         # title excluding the destination directory path
         title = output[len(self.output) :]
